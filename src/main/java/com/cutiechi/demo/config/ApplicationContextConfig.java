e36e0aee50cf82665ff6a0298d30fa904dc24363
cutiechi <chi@cutie.ooo>
2018-09-14 10:21:55 +0800

    feat(config): add spring application context component scan base package config

--- src/main/java/com/cutiechi/demo/config/ApplicationContextConfig.java
+++ src/main/java/com/cutiechi/demo/config/ApplicationContextConfig.java
@@ -1,8 +1,15 @@
 package com.cutiechi.demo.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Spring Application Context 配置类, 相当于 applicationContext.xml
+ *
+ * @author Cutie Chi
+ */
 @Configuration
+@ComponentScan("com.cutiechi.demo")
 public class ApplicationContextConfig {
 
 }
