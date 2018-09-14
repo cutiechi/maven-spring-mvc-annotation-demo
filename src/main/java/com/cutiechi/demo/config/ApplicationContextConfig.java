741dcd808a6042f7dc6f0cb0a757709b3ccf02fe
cutiechi <chi@cutie.ooo>
2018-09-14 10:23:31 +0800

    feat(config): add spring application context component scan exclude filter config

--- src/main/java/com/cutiechi/demo/config/ApplicationContextConfig.java
+++ src/main/java/com/cutiechi/demo/config/ApplicationContextConfig.java
@@ -1,7 +1,11 @@
 package com.cutiechi.demo.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Spring Application Context 配置类, 相当于 applicationContext.xml
@@ -9,7 +13,15 @@ import org.springframework.context.annotation.Configuration;
  * @author Cutie Chi
  */
 @Configuration
-@ComponentScan("com.cutiechi.demo")
+@ComponentScan(
+    basePackages = {
+        "com.cutiechi.demo"
+    },
+    excludeFilters = {
+        @Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class),
+        @Filter(type = FilterType.ANNOTATION, value = Controller.class)
+    }
+)
 public class ApplicationContextConfig {
 
 }
